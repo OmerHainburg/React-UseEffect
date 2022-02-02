f4c970642bc53763bd235845ff0c1d359f97113d
Your Name <you@example.com>
2022-02-02 13:03:57 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Counter from './Counter';
+import CleanUp from './CleanUp';
+import Timer from './Timer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h3>React UseAffect</h3>
+       <h1>React UseAffect</h1>
+       <h2>Function Component Effects</h2>
+       <Counter />
+       <h2>Clean Up Effects</h2>
+       <CleanUp />
+       <h2>Control When Effects Are Called</h2>
+       <Timer />
+       <br></br>
       </header>
     </div>
   );
